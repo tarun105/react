9f47594a6ed5eb466dd3364694bf21b68266638e
Tarun Waghmare <Tarun.Waghmare@vertisystem.local>
2024-01-04 16:44:17 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>React app Deployment Free hosting</h1>
+          <p>if you like this video click on below and subscribe us</p>
+          <button><a href="https://www.youtube.com/@JavaScriptFolks/videos">Click me!</a></button>
+          <h1>This is last video of this playlist</h1>
+    </div>
+  );
+}
+
+export default App;
